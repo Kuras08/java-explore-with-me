890db48632c0065cb0fd6bcb9870404bb8e28e1b
Евгений <kurasovevgeni@yandex.ru>
2025-07-17 12:08:43 +0300

    добавил сервис статистики

---
+++ stats/stats-server/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO stats (app, uri, ip, time_stamp)
+VALUES ('ewm-main-service', '/events/1', '192.168.0.1', '2025-07-17 10:00:00'),
+       ('ewm-main-service', '/events/1', '192.168.0.2', '2025-07-17 10:05:00'),
+       ('ewm-main-service', '/events/2', '192.168.0.3', '2025-07-17 11:00:00');
